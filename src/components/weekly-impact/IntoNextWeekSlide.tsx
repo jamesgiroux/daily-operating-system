5b6c2f830543f881f6e7fcf4f76f253069279f17
James Giroux <james.giroux@automattic.com>
2026-03-21 21:50:13 -0400

    Consolidate editorial design system primitives

--- src/components/weekly-impact/IntoNextWeekSlide.tsx
+++ src/components/weekly-impact/IntoNextWeekSlide.tsx
@@ -33,15 +33,8 @@ export function IntoNextWeekSlide({ content, onUpdate }: IntoNextWeekSlideProps)
   return (
     <section
       id="next-week"
-      style={{
-        scrollMarginTop: 60,
-        minHeight: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        padding: "120px 120px 80px",
-        scrollSnapAlign: "start",
-      }}
+      className="report-surface-slide"
+      style={{ scrollMarginTop: 60 }}
     >
       {/* Overline */}
       <div
